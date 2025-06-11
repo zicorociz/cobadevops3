dd78c82a164de1f535778d156861dc6f209213c9
zicorociz <khaliszikrul16@gmail.com>
2025-06-11 21:24:51 +0700

    update lint

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   verbose: true,
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  testEnvironment: "jsdom",  // Menentukan jsdom untuk pengujian React
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom', // Menentukan jsdom untuk pengujian React
   setupFilesAfterEnv: [
-    "@testing-library/jest-dom/extend-expect"  // Memastikan toBeInTheDocument tersedia
+    '@testing-library/jest-dom/extend-expect', // Memastikan toBeInTheDocument tersedia
   ],
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",  // Menggunakan babel-jest untuk transformasi
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Menggunakan babel-jest untuk transformasi
   },
-  moduleFileExtensions: ["js", "jsx"],
+  moduleFileExtensions: ['js', 'jsx'],
 };
