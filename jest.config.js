5084f531ffac6ffcf2ed3c7d783f124be4c0ebe0
zicorociz <khaliszikrul16@gmail.com>
2025-06-11 21:08:34 +0700

    tambahin jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",  // Menentukan jsdom untuk pengujian React
+  setupFilesAfterEnv: [
+    "@testing-library/jest-dom/extend-expect"  // Memastikan toBeInTheDocument tersedia
+  ],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",  // Menggunakan babel-jest untuk transformasi
+  },
+  moduleFileExtensions: ["js", "jsx"],
+};
